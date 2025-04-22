44fa1fb4c193bd5af279cc8cb4cb520a52f804c7
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-04-22 18:50:42 +0200

    Flask connection: introducing print_question and lifeline functions

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for
 # url_for is used in base.html to generate URLs for static files
 
-from game_logic import print_question, get_available_questions, read_question
+from game_logic import print_question, get_available_questions, read_question, lifeline
+import random
 
 app = Flask(__name__)
 
@@ -17,7 +18,14 @@ def index():
         A rendered HTML template for the index page.
         And displays a question from the available questions.
     """
-    return render_template('index.html')
+    read_question()
+    old_questions = set()
+    all_questions = read_question()
+
+    available = get_available_questions(all_questions, old_questions)
+    current_entry = random.choice(available)
+    old_questions.add(current_entry["ID"])
+    return render_template('index.html', question = print_question(current_entry), lifeline = lifeline())
 
 
 if __name__ == "__main__":
