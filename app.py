d34ad7851bcd7b86d2431a06c36aee2de94ed84d
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-04-28 14:00:41 +0200

    Flask connection: Display a question from JSON-file in HTML-file
    A question from the JSON-file is now displayed in the HTML-file. Reloading the page cause a new question to display.

--- app.py
+++ app.py
@@ -6,7 +6,31 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+def print_a_question_from_file():
+    """
+    Selects and returns a random available question from a file.
+
+    Reads all questions from a file, filters out questions that have already been asked,
+    and randomly selects one from the remaining available questions. If no questions are available,
+    a message is printed to inform the user. The selected question's ID is added to the set of
+    previously asked questions to avoid repetition.
+
+    Returns:
+        str: The text of the selected question.
+    """
+    old_questions = set()
+    all_questions = read_question()
+    
+    available = get_available_questions(all_questions, old_questions)
+        
+    if not available:
+        print("\nThere's no questions left. Add more! And you win i guess?")
+
+    current_entry = random.choice(available)
+    old_questions.add(current_entry["ID"])
+    return current_entry["question"]
+
+@app.route('/', methods=['GET'])
 def index():
     """
     Route handler for the home page.
@@ -18,22 +42,9 @@ def index():
         A rendered HTML template for the index page.
         And displays a question from the available questions.
     """
-    old_questions = set()
-    all_questions = read_question()
+    question = print_a_question_from_file()
 
-    while True:
-        available = get_available_questions(all_questions, old_questions)
-        
-        if not available:
-            print("\nThere's no questions left. Add more! And you win i guess?")
-            break
-
-        current_entry = random.choice(available)
-        old_questions.add(current_entry["ID"])
-        question = print_question(current_entry)
-        life = lifeline()
-
-    return render_template('index.html', question = question, lifeline = life)
+    return render_template('index.html', question = question)
 
 
 if __name__ == "__main__":
